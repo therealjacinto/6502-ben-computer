987b3fae7f3a679120167d0b7a0f06e993be62ca
therealajcinto <therealjacinto@gmail.com>
2020-08-19 17:59:08 -0700

    ENH: startup code now closely matches asm code

--- targets/libsrc/6502-ben/startup.c
+++ targets/libsrc/6502-ben/startup.c
@@ -1,29 +1,45 @@
 #include "lcdio/lcdio.h"
+
 #define NULL ((void *)0)
 
+// Stack End (__SE)is defined in linker
+extern int* _SE;
+
+// Used by compiler
+void set_user_sp_address(__reg("a/x") int** se) =
+  "\tsta sp\n"
+  "\tstx sp+1\n";
+
+// Hardware stack pointer
+void set_hardware_sp_address(__reg("a") char val) =
+  "\ttax\n"
+  "\ttxs";
+
 extern int main(int argc, char **argv);
-void set_user_sp() = "\tlda  #<__SE\n\tsta  sp\n\tlda  #>__SE\n\tsta  sp+1";
-void set_system_sp() = "\tldx  #0xff\n\ttxs";
-void goto_start() = "\tjmp _start";
-void goto_exit() = "\tjmp _exit";
+
+// Is called once main completes
+void exit()
+{
+    // jmp to exit indefinitely
+    exit();
+}
+
+// After init is called, this handles the call to main and then exit.
+void start()
+{
+    main(0, NULL);
+    // Call exit
+    exit();
+}
 
 #pragma section init
 void init1()
 {
-    set_user_sp();
-    set_system_sp();
+    set_user_sp_address(&_SE);
+    set_hardware_sp_address(0xff);
     lcd_reset();
-    goto_start();
+    // Call start() without returning
+    start();
 }
 
-#pragma section text
-void start()
-{
-    main(0, NULL);
-    goto_exit();
-}
-
-void exit()
-{
-    goto_exit();
-}
+#pragma section default
